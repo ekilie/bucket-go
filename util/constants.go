64d3072602aa58d77a6febb7b31a9fafadebbe49
Tachera Sasi <tacherasasi@gmail.com>
2025-10-28 13:40:35 +0300

    Refactor AllowedExtensions map to improve organization and readability by grouping file types

--- util/constants.go
+++ util/constants.go
@@ -11,12 +11,15 @@ const MaxFileSize = 100 * 1024 * 1024
 
 // AllowedExtensions is the set of allowed file extensions (lowercase).
 var AllowedExtensions = map[string]bool{
+	// Images
 	".jpg":  true,
 	".jpeg": true,
 	".png":  true,
 	".gif":  true,
 	".webp": true,
 	".svg":  true,
+
+	// Documents
 	".pdf":  true,
 	".txt":  true,
 	".doc":  true,
@@ -25,10 +28,25 @@ var AllowedExtensions = map[string]bool{
 	".xlsx": true,
 	".ppt":  true,
 	".pptx": true,
+
+	// Archives
 	".zip":  true,
 	".rar":  true,
 	".tar":  true,
 	".gz":   true,
+
+	// Data
 	".json": true,
 	".xml":  true,
+
+	// Audio
+	".mp3":  true,
+	".wav":  true,
+	".m4a":  true,
+	".aac":  true,
+	".ogg":  true,
+	".oga":  true,
+	".flac": true,
+	".opus": true,
+	".webm": true,
 }
